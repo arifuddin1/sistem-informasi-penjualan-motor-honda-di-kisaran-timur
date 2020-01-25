bc92dcb5d95e1ea73a723d954148df391ae0486f
arifuddin1 <60189586+arifuddin1@users.noreply.github.com>
2020-01-25 12:08:50 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+            <div class="alert alert-success">
+               <center> <strong>Selamat Datang Kembali</strong> </center>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Penjualan Motor Honda Kisaran</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/download.png" width="250" height="250"></img></p>
+                          <p class="label-title" align="center"><strong>CV. Honda Karya Utama</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Penjualan Motor Honda Kisaran.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data Penjualan Motor Honda<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data Penjualan.<br>
+                            Untuk mengelola Data Penjualan Motor, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12"><center><font face = "nyala">
+            Copyright &COPY; <?= date('Y')?> Rini Anugrah H.s, Amalia Lestari, Ahmad Yani Sirait |
+            <a href="#">Dokumentasi</a>
+            <a href="#">Peta Situs</a>
+            <a href="#">Support</a>
+			<center></font>
+        </div>
+    </div>
+</div>
